fa9ecf83875e30dc5e5d82965d9a4051d19e2501
Alejandro Villa <vcalejandro97@gmail.com>
2023-07-07 12:43:30 -0500

    Refactoring Actions

--- Sources/ForgeUIPackage/Common/Action.swift
+++ Sources/ForgeUIPackage/Common/Action.swift
@@ -7,17 +7,48 @@
 
 import Foundation
 
-public enum ActionType: String, Codable {
-    case dismiss = "DISMISS"
-    case deviceAuth = "DEVICE_AUTH"
-    case login = "LOGIN"
-    case forgotPassword = "FORGOT_PASSWORD"
-}
-
 public struct Action: Codable {
     public let type: ActionType
 
     public init(type: ActionType) {
         self.type = type
+    }
+}
+
+public enum ActionType: Codable, RawRepresentable, Equatable {
+    case deviceAuth
+    case dismiss
+    case action(String)
+
+    enum CodingKeys: String, CodingKey {
+        case dismiss = "DISMISS"
+        case deviceAuth = "DEVICE_AUTH"
+    }
+
+    public var rawValue: String {
+        switch self {
+        case .deviceAuth: return CodingKeys.deviceAuth.rawValue
+        case .dismiss: return CodingKeys.dismiss.rawValue
+        case .action(let rawValue): return rawValue
+        }
+    }
+
+    public init?(rawValue: String) {
+        switch CodingKeys(rawValue: rawValue.uppercased()) {
+        case .deviceAuth: self = .deviceAuth
+        case .dismiss: self = .dismiss
+        default: self = .action(rawValue)
+        }
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let rawValue = try container.decode(String.self)
+        self = .init(rawValue: rawValue) ?? .action(rawValue)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(rawValue)
     }
 }
