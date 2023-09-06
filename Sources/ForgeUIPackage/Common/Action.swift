e539e7aeb4df7a610ba40a81d960718a283b51a4
Alejandro Villa <vcalejandro97@gmail.com>
2023-09-05 23:43:59 -0500

    Adding closeBottomSheet action

--- Sources/ForgeUIPackage/Common/Action.swift
+++ Sources/ForgeUIPackage/Common/Action.swift
@@ -9,15 +9,18 @@ import Foundation
 
 public enum Action: Codable, RawRepresentable, Equatable {
     case dismiss
+    case closeBottomSheet
     case action(String)
 
     enum CodingKeys: String, CodingKey {
         case dismiss = "DISMISS"
+        case closeBottomSheet = "CLOSE_BOTTOM_SHEET"
     }
 
     public var rawValue: String {
         switch self {
         case .dismiss: return CodingKeys.dismiss.rawValue
+        case .closeBottomSheet: return CodingKeys.closeBottomSheet.rawValue
         case .action(let rawValue): return rawValue
         }
     }
@@ -25,6 +28,7 @@ public enum Action: Codable, RawRepresentable, Equatable {
     public init?(rawValue: String) {
         switch CodingKeys(rawValue: rawValue.uppercased()) {
         case .dismiss: self = .dismiss
+        case .closeBottomSheet: self = .closeBottomSheet
         default: self = .action(rawValue)
         }
     }
